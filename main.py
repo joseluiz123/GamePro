a8353ed014f79bbfde4bf8f7a72501bf3192f4b2
joseluiz123 <joseluiz140697@gmail.com>
2021-09-18 15:45:42 -0300

    Funcionando e contando pontos

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
 
-def main():
-    print('####################')
-    print('### Quiz sobre programação ###')
-    print('####################')
-    print()
 
+
+def main():
+    print('##############################')
+    print('### Quiz sobre programação ###')
+    print('##############################')
+    print()
+    pontos = 0
     request = requests.get('https://raw.githubusercontent.com/joseluiz123/GamePro/main/pergunta1.json')
 
     address_data = request.json()
@@ -13,15 +15,21 @@ def main():
 
     if 'erro' not in address_data:
 
-        print('Pergunta: {}'.format(address_data['pergunta']))
+        for i in range(1,3):
+            pergunta = address_data[f'pergunta{i}']
+            #print('Pergunta: {}'.format(address_data['pergunta']))
+            print(f'Pergunta {i}: ' + pergunta)
 
-        resposta = input("Digite a resposta: ")
-        resposta_correta = address_data['resposta_correta']
+            resposta = input("Digite a resposta: ")
+            resposta_correta = address_data[f'resposta_correta{i}']
 
-        if resposta == resposta_correta:
-            print("Parabéns vc acertou!")
-        else:
-            print("Resposta errada")
+            if resposta == resposta_correta:
+                print("Parabéns vc acertou!")
+                pontos = pontos + 10
+            else:
+                print("Resposta errada")
+            print('===================')
+        print(f'Você conseguiu fazer: {pontos}')
 
     else:
         print('Desculpe, aconteceu algum erro, tente executar novamente.')
@@ -30,3 +38,13 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+'''Um desenvolvedor implementou um programa para exibir a média de um dado retirado de uma grande base de dados. Para isso, foi utilizada a linguagem Python. O trecho do código que mostra o resultado é apresentado a seguir. Assinale a alternativa correta acerca desse trecho de código sabendo que a média do usuário foi 75.
+
+print('Sua média foi {}.'.format(med))
+
+A O programa imprime: Sua média foi 75.
+B O programa imprime: Sua média foi {}.
+C O programa imprime: Sua média foi Null.
+D O programa apresenta um erro na impressão porque tenta converter tipo numérico em caractere.
+E O programa apresenta um erro na impressão, pois não apresenta o formato do valor.'''
