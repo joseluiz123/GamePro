b05e9f367e5c6a8760664b8f116e8361d292385f
joseluiz123 <joseluiz140697@gmail.com>
2021-10-04 15:41:30 -0300

    Update personagem.py
    contando pontos ao colidir com a resposta correta

--- personagem.py
+++ personagem.py
@@ -7,23 +7,27 @@ pygame.init()
 
 pygame.mixer.music.set_volume(0.1)
 musica_de_fundo = pygame.mixer.music.load(
-    'audio/10-Overworld-Bgm.mp3')  # Site com os sons https://downloads.khinsider.com/game-soundtracks/album/super-mario-world-original-soundtrack
+    'audio/10-overworld-bgm.mp3')  # site com os sons https://downloads.khinsider.com/game-soundtracks/album/super-mario-world-original-soundtrack
 pygame.mixer.music.play(-1)
 
-request = requests.get('https://raw.githubusercontent.com/joseluiz123/GamePro/main/pergunta1.json')
+request = requests.get('https://raw.githubusercontent.com/joseluiz123/gamepro/main/pergunta1.json')
 address_data = request.json()
+
+pontos = 0
 
 pergunta = address_data[f'pergunta1']
 resposta = address_data[f'resposta_correta1']
+resposta_errada1 = address_data[f'resposta_errada1']
+resposta_errada2 = address_data[f'resposta_errada2']
+
 # print(f'Pergunta:' + pergunta)
-game_font = pygame.font.Font(None, 28)
+game_font = pygame.font.SysFont('arial', 25, False, False)
 
-if len(pergunta) > 20:
-    pergunta = pergunta.split('*', 2)
-    for x in range(2):
-        print(pergunta[x])
+pergunta = pergunta.split('*', 2)
+for x in range(2):
+    print(pergunta[x])
 
-texto = f'Pergunta 1: {pergunta[0]}'
+texto = f'pergunta 1: {pergunta[0]}'
 texto_formatado = game_font.render(texto, True, (0, 0, 0))
 
 texto2 = f'{pergunta[1]}'
@@ -33,16 +37,22 @@ texto_formatado2 = game_font.render(texto2, True, (0, 0, 0))
 resposta = f'{resposta}'
 resposta_formatada = game_font.render(resposta, True, (0, 0, 0))
 
+resposta_errada1 = f'{resposta_errada1}'
+resp_errada1_formatada = game_font.render(resposta_errada1, True, (0, 0, 0))
+
+resposta_errada2 = f'{resposta_errada2}'
+resp_errada2_formatada = game_font.render(resposta_errada2, True, (0, 0, 0))
+
 largura = 760
 altura = 480
 
-PRETO = (0, 0, 0)
+preto = (0, 0, 0)
 
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Sprites')
+pygame.display.set_caption('sprites')
 
 
-class Personagem(pygame.sprite.Sprite):
+class personagem(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.som_pulo = pygame.mixer.Sound('audio/pulo_yoshi.mp3')
@@ -51,7 +61,7 @@ class Personagem(pygame.sprite.Sprite):
         self.sprites.append(pygame.image.load('imagens/sprite_0.png'))
         self.sprites.append(pygame.image.load('imagens/sprite_1.png'))
         self.sprites.append(pygame.image.load('imagens/sprite_2.png'))
-        '''self.sprites.append(pygame.image.load('imagens/sprite_3.png'))
+        ''' self.sprites.append(pygame.image.load('imagens/sprite_3.png'))
         self.sprites.append(pygame.image.load('imagens/sprite_4.png'))
         self.sprites.append(pygame.image.load('imagens/sprite_5.png'))'''
         self.atual = 0
@@ -60,12 +70,11 @@ class Personagem(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
         self.pos_y_inicial = 368
-        self.rect.topleft = 320, 368  # 320, 255 posição x e y do personagem
+        self.rect.topleft = 320, 368  # 320, 368 posição x e y do personagem
         self.pulo = False
         self.andar_frente = False
         self.andar_tras = False
-        self.volta_meio = False
-        self.volta_meio_d = False
+        self.colidiu = False
 
     def andar_para_tras(self):
         self.andar_tras = True
@@ -73,15 +82,13 @@ class Personagem(pygame.sprite.Sprite):
     def andar_para_frente(self):
         self.andar_frente = True
 
-    def voltar_ao_meio(self):
-        self.volta_meio = True
-
-    def voltar_ao_meio_d(self):
-        self.volta_meio_d = True
-
     def pular(self):
         self.pulo = True
         self.som_pulo.play()
+
+    def colidiu(self):
+        self.colidiu = True
+        #personagem.coliderec
 
     def update(self):
         # personagem.rect.x = 320
@@ -110,7 +117,23 @@ class Personagem(pygame.sprite.Sprite):
             else:
                 self.rect.x += 10
 
-        # não tava comentado self.atual = self.atual + 0.5
+        '''if pygame.sprite.groupcollide(todas_as_sprites, grupo_resp_correta):
+            print('colidiu')'''
+        '''  #teste alterar sprite
+        self.atual = self.atual + 0.5
+        if self.atual >= len(self.sprites):
+            #if self.andar_frente == True:
+            self.atual = 0
+        if self.andar_frente == True:
+            self.atual = 4
+        print(self.andar_frente)
+        print(self.atual)
+
+        self.image = self.sprites[int(self.atual)]
+        self.image = pygame.transform.scale(self.image, (10 * 7, 10 * 7))'''
+        #print(len(self.sprites))
+        #print(self.atual)
+
 
         ### tentativa de alterar a sprite qudno estiver andando para trás ###
         '''if self.andar_tras == True:
@@ -140,7 +163,7 @@ class Personagem(pygame.sprite.Sprite):
 
             self.image = pygame.transform.scale(self.image, (10 * 7, 10 * 7))
 
-        #altera a sprite para movimentando para frente  ==REVISAR==
+        #altera a sprite para movimentando para frente  ==revisar==
         else: # self.andar_frente == True
             self.atual = self.atual + 0.5
             print(self.atual)
@@ -150,14 +173,37 @@ class Personagem(pygame.sprite.Sprite):
 
             self.image = pygame.transform.scale(self.image, (10 * 7, 10 * 7))'''
 
+        '''funcionando '''
         self.atual = self.atual + 0.5
         if self.atual >= len(self.sprites):
             self.atual = 0
         self.image = self.sprites[int(self.atual)]
         self.image = pygame.transform.scale(self.image, (10 * 7, 10 * 7))
 
+class resposta_correta(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprites = []
+        self.sprites.append(pygame.image.load('imagens/resposta_correta.png'))
+        '''self.sprites.append(pygame.image.load('imagens/sprite_1.png'))
+        self.sprites.append(pygame.image.load('imagens/sprite_0.png'))'''
+        self.atual = 0
+        self.image = self.sprites[self.atual]
+        self.image = pygame.transform.scale(self.image, (150, 35))  # (32 * 7, 32 * 7)
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = 300, 242 #300, 265
+
+    def update(self):
+        print('atualizou')
+
 todas_as_sprites = pygame.sprite.Group()
-personagem = Personagem()
+
+resposta_correta = resposta_correta() #não existia
+grupo_resp_correta = pygame.sprite.Group() #não existia
+grupo_resp_correta.add(resposta_correta) #não existia
+
+personagem = personagem()
 todas_as_sprites.add(personagem)
 
 pygame.draw.rect(tela, (0, 255, 0), (10, 5, 100, 20))
@@ -172,7 +218,7 @@ relogio = pygame.time.Clock()
 
 while True:
     relogio.tick(30)
-    tela.fill(PRETO)
+    tela.fill(preto)
 
     pygame.event.pump()
     keys = pygame.key.get_pressed()
@@ -198,86 +244,31 @@ while True:
 
     pygame.draw.rect(imagem_fundo, (255, 245, 245), (25, 10, 700, 210))
 
+    #verifica se o personagem colidiu na resposta
+    colisao = pygame.sprite.spritecollide(personagem, grupo_resp_correta, False)
+    if colisao:
+        grupo_resp_correta.update()  # não existia
+        pontos += 1
+        pass
+        print('colidiu')
+        print(pontos)
+
     tela.blit(imagem_fundo, (0, 0))
     # tela.blit(nuvem, (10, -30))
 
+    grupo_resp_correta.draw(tela)  # não existia
+
     tela.blit(texto_formatado, (30, 15))  # exibe a pergunta
     tela.blit(texto_formatado2, (30, 50))  # exibe a pergunta
-    tela.blit(resposta_formatada, (320, 270))  # exibe a resposta
+    tela.blit(resposta_formatada, (320, 245))  # exibe a resposta
+    tela.blit(resp_errada1_formatada, (80, 250))  # exibe a resposta errada 1
+    tela.blit(resp_errada2_formatada, (600, 260))  # exibe a resposta errada 2
+
+    '''if personagem.image.spritecollideany(resposta_formatada):
+        print('colidiu na resposta')'''
+
+
 
     todas_as_sprites.draw(tela)
     todas_as_sprites.update()
     pygame.display.flip()
-
-''' FUNCIONANDO
-import pygame
-from pygame.locals import *
-from sys import exit
-
-pygame.init()
-
-largura = 760
-altura = 480
-
-PRETO = (0, 0, 0)
-
-tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Sprites')
-
-
-class Personagem(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.sprites = []
-        self.sprites.append(pygame.image.load('homem_terno/homem_terno0.png'))
-        self.sprites.append(pygame.image.load('homem_terno/homem_terno1.png'))
-        self.sprites.append(pygame.image.load('homem_terno/homem_terno2.png'))
-        self.atual = 0
-        self.image = self.sprites[self.atual]
-        self.image = pygame.transform.scale(self.image, (32 * 7, 32 * 7))
-
-        self.rect = self.image.get_rect()
-        self.rect.topleft = 300, 255
-
-    def update(self):
-        self.atual = self.atual + 0.5
-        if self.atual >= len(self.sprites):
-            self.atual = 0
-        self.image = self.sprites[int(self.atual)]
-        self.image = pygame.transform.scale(self.image, (32 * 7, 32 * 7))
-
-
-todas_as_sprites = pygame.sprite.Group()
-personagem = Personagem()
-todas_as_sprites.add(personagem)
-
-imagem_fundo = pygame.image.load('cidade_fundo.jpg').convert()
-imagem_fundo = pygame.transform.scale(imagem_fundo, (largura, altura))
-
-relogio = pygame.time.Clock()
-
-while True:
-    relogio.tick(30)
-    tela.fill(PRETO)
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            exit()
-
-    tela.blit(imagem_fundo, (0, 0))
-    todas_as_sprites.draw(tela)
-    todas_as_sprites.update()
-    pygame.display.flip()
-
-
-
-{
-  "pergunta1": "É correto afirmar que a definição de variáveis em PHP * é feita inserindo o caractere $ no início do nome da variável",
-  "resposta_correta1": "verdadeiro",
-  "pergunta2": "As tuplas, embora sejam semelhantes às listas, estão limitadas a, no máximo, cinco níveis.",
-  "resposta_correta2": "falso",
-  "pergunta3": "Um desenvolvedor implementou um programa para exibir a média de um dado retirado de uma grande base de dados. \n Para isso, foi utilizada a linguagem Python. O trecho do código que mostra o resultado é apresentado a seguir.\n Assinale a alternativa correta acerca desse trecho de código sabendo que a média do usuário foi 75.\n print('Sua média foi {}.'.format(med)) \n (A) O programa imprime: Sua média foi 75. \n (B) O programa imprime: Sua média foi {}. \n (C) O programa imprime: Sua média foi Null. \n (D) O programa apresenta um erro na impressão porque tenta converter tipo numérico em caractere. \n (E) O programa apresenta um erro na impressão, pois não apresenta o formato do valor.",
-  "resposta_correta3": "A"
-}
-
-'''
